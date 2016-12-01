66c78bf58f33628389296c61981c2a8565326cef
Tim Rietdijk <tim.is@live.nl>
2016-12-01 17:12:24 +0100

    second commit

---
+++ Assets/CameraControler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControler : MonoBehaviour {
+
+    public GameObject Target;
+    public float Speed;
+
+    private Vector3 TargetPosition;
+
+    void Update () {
+        TargetPosition = new Vector3(Target.transform.position.x, Target.transform.position.y, -6.5f);
+        transform.position = Vector3.Lerp(transform.position, TargetPosition, Speed * Time.deltaTime);            
+	}
+}
