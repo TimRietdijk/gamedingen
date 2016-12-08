4ad856effe7c77420cd0bb7c4bfa56f526694514
Tim Rietdijk <tim.is@live.nl>
2016-12-08 21:29:42 +0100

    sprite update

---
+++ Assets/numbers.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class numbers : MonoBehaviour {
+    public int number = 0;
+    public Text counter;
+	// Use this for initialization
+	void Start () {
+        counter.text = number.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        counter.text = "number: " + number.ToString();
+	}
+
+    public void increase() {
+        number++;
+    }
+}
